c41126dd3690e92aadd13b21e784a2f85e7d91ef
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-09-16 00:13:01 +0700

    Adding add a new dataset and delete dataset routes

--- routes/dataset.js
+++ routes/dataset.js
@@ -7,6 +7,8 @@ const jwt = express_jwt({
   algorithms: ["HS256"],
 });
 
+const { body, validationResult } = require("express-validator");
+
 const DatasetIndex = require("../model/DatasetIndex");
 const Dataset = require("../model/Dataset");
 const Device = require("../model/Device");
@@ -42,6 +44,48 @@ router.get("/", jwt, async (req, res) => {
   );
 });
 
+router.post(
+  "/",
+  jwt,
+  body("name").isLength({ min: 5 }),
+  body("type").isLength({ min: 5, max: 6 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    if (!(req.body.type == "number" || req.body.type == "image"))
+      return res.status(422).json({ message: "Invalid dataset type." });
+
+    const newDatasetIndex = new DatasetIndex({
+      user_id: req.user.user_id,
+      name: req.body.name,
+      description: req.body.description,
+      type: req.body.type,
+    });
+
+    newDatasetIndex.save((err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          message: "Server error, failed to save the new device.",
+        });
+      }
+
+      res.json({
+        message: "The new dataset has been successfully saved.",
+        saved_dataset: {
+          name: doc.name,
+          description: doc.description,
+          type: doc.type,
+          createdAt: doc.createdAt,
+        },
+      });
+    });
+  }
+);
+
 router.get("/:dataset_id", jwt, async (req, res) => {
   const devices = await Device.find({ user_id: req.user.user_id }).exec();
   Dataset.find(
@@ -73,6 +117,29 @@ router.get("/:dataset_id", jwt, async (req, res) => {
       res.json(data);
     }
   );
+});
+
+router.delete("/:dataset_id", jwt, async (req, res) => {
+  const datasetIndex = await DatasetIndex.findOne({
+    _id: req.params.dataset_id,
+  })
+    .exec()
+    .catch(() => res.status(422).json({ message: "Invalid dataset." }));
+
+  if (!datasetIndex) {
+    res.status(404).json({ message: "Dataset not found." });
+    return;
+  }
+
+  DatasetIndex.deleteOne(datasetIndex, (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Server error, failed to delete the dataset." });
+    }
+
+    return res.json({ message: "The dataset has been successfully deleted." });
+  });
 });
 
 router.get("/:dataset_id/device", jwt, async (req, res) => {
